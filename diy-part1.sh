2266d4b84dbf6aa921d426030c031d248de8052b
lzr1264823628 <50105984+lzr1264823628@users.noreply.github.com>
2020-06-07 11:20:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,3 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb' feeds.conf.default
-sed -i '$a src-git luci-app-vssr https://github.com/jerrykuku/luci-app-vssr' feeds.conf.default
-sed -i '$a src-git OpenAppFilter https://github.com/destan19/OpenAppFilter package/OpenAppFilter' feeds.conf.default
